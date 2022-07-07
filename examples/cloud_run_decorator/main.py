11759c1404ad4ecdb7cf71504482be7dc1f32f55
mapp190413 <mapp190413@sri.ad>
2022-07-07 15:31:37 -0500

    Adda aññ profmen amd hrerader

--- examples/cloud_run_decorator/main.py
+++ examples/cloud_run_decorator/main.py
@@ -36,8 +36,8 @@ def hello_http(request):
         # header and caches preflight response for an 3600s
         headers = {
             'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
-            'Access-Control-Allow-Headers': 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers',
+            'Access-Control-Allow-Methods': '*',
+            'Access-Control-Allow-Headers': '*',
             'Access-Control-Max-Age': '3600'
         }
 
