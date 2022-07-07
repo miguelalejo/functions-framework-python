045b19738604ad47d8ad80db41fe77a7f3f0c990
mapp190413 <mapp190413@sri.ad>
2022-07-07 14:51:18 -0500

    Add orignre accest

--- examples/cloud_run_decorator/main.py
+++ examples/cloud_run_decorator/main.py
@@ -37,7 +37,7 @@ def hello_http(request):
         headers = {
             'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
-            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Headers': 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers',
             'Access-Control-Max-Age': '3600'
         }
 
