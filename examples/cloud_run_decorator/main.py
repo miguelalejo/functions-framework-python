55f19a013cfb1eef75232eb9463b9894ef4fc320
mapp190413 <mapp190413@sri.ad>
2022-07-07 14:20:46 -0500

    add corss domain

--- examples/cloud_run_decorator/main.py
+++ examples/cloud_run_decorator/main.py
@@ -19,8 +19,57 @@ import flask
 from flask import escape
 from flask import abort
 import requests
+from flask import Flask
+from flask_cors import CORS
+
+from flask import Flask, make_response, request, current_app
+from datetime import timedelta
+from functools import update_wrapper
+
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
+
 
 @functions_framework.http
+@crossdomain(origin="*")
 def hello_http(request):
     """ Responds to a GET request with "Hello world!". Forbids a PUT request.
     Args:
@@ -37,14 +86,7 @@ def hello_http(request):
         'Access-Control-Allow-Headers': 'Content-Type',
         'Access-Control-Max-Age': '3600'
     }
-    
-    if request.method == 'OPTIONS':
-        user = requests.get('https://functions-framework-python-pcqrvbtxdq-uc.a.run.app'
-                        ).json()
-        response = flask.jsonify(user)       
-        response.headers.set('Access-Control-Allow-Origin', '*')
-        response.headers.set('Access-Control-Allow-Methods', 'GET, POST')
-        return response   
+
     if request.method == 'GET':
         return (hello_http_get(request), 200, headers)
     elif request.method == 'POST':
