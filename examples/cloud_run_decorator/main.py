b4e18b95046a38bb78772756a4e6307f37ce25d7
mapp190413 <mapp190413@sri.ad>
2022-07-07 11:23:05 -0500

    smaplke POST

--- examples/cloud_run_decorator/main.py
+++ examples/cloud_run_decorator/main.py
@@ -25,3 +25,27 @@ def hello_cloud_event(cloud_event):
 @functions_framework.http
 def hello_http(request):
     return "Hello world! My Friends"
+
+@functions_framework.http
+def hello_http(request):
+    """HTTP Cloud Function.
+    Args:
+        request (flask.Request): The request object.
+        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`
+        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
+    """
+    print("POST START")
+    request_json = request.get_json(silent=True)
+    print(request_json)
+    request_args = request.args
+
+    if request_json and 'name' in request_json:
+        name = request_json['name']
+    elif request_args and 'name' in request_args:
+        name = request_args['name']
+    else:
+        name = 'World'
+    return 'Hello {}!'.format(escape(name))
