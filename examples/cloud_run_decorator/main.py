83966759453dfa578711b1f52d540f92b5661d1f
mapp190413 <mapp190413@sri.ad>
2022-07-26 08:48:44 -0500

    addd conver Json

--- examples/cloud_run_decorator/main.py
+++ examples/cloud_run_decorator/main.py
@@ -25,11 +25,14 @@ from google.cloud import pubsub_v1
 from MongoService import *
 import json
 from bson import ObjectId
+from datetime import date, datetime
 
 class JSONEncoder(json.JSONEncoder):
     def default(self, o):
         if isinstance(o, ObjectId):
             return str(o)
+        if isinstance(o, (datetime, date)):
+            return o.isoformat()
         return json.JSONEncoder.default(self, o)
     
 
